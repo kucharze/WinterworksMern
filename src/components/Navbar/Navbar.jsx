1e7858ba8bf809277c53fc7ec54642e0eb790b0c
Zachary Kuchar <zacharyk323@live.com>
2023-09-11 12:58:08 -0400

    Added navbar css module

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css'
 
 function Navbar() {
   return (
-    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-evenly'}}>
+    <div className={styles.Navbar}>
       <Link>Designs</Link>
       <Link>Products</Link>
       <h2>page3</h2>
