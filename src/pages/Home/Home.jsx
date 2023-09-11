f2f38ceaf4c848f4184ddcdb982b700a0da53a3f
Zachary Kuchar <zacharyk323@live.com>
2023-09-11 14:21:01 -0400

    Added Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>The home page</h1>
+        <h4>Welcome to Winterworks</h4>
+        <h4>IDK what to put here: Maybe a picture</h4>
+    </div>
+  )
+} 
+
+export default Home
