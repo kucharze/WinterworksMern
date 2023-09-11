8bc648c06b536f5adcc98f6dcfced7090c861715
Zachary Kuchar <zacharyk323@live.com>
2023-09-11 12:50:06 -0400

    Changed Navbar link names

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,8 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-evenly'}}>
-      <h1>The Navbar component</h1>
-      <h2>page1</h2>
-      <h2>page2</h2>
+      <Link>Designs</Link>
+      <Link>Products</Link>
       <h2>page3</h2>
     </div>
   )
