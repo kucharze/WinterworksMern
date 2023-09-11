9a7053c4f6fe46186b1ce4907beb4798456f3615
Zachary Kuchar <zacharyk323@live.com>
2023-09-11 11:33:53 -0400

    Added a navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1>Winterworks</h1>
+      <Navbar />
       <h4>Welcome to Winterworks</h4>
       <h4>IDK what to put here: Maybe a picture</h4>
     </div>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-evenly'}}>
+      <h1>The Navbar component</h1>
+      <h2>page1</h2>
+      <h2>page2</h2>
+      <h2>page3</h2>
+    </div>
+  )
+}
+
+export default Navbar
