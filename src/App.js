c34d3e21e7b5a79bc38f8a0eb93c84b83bbb507d
Zachary Kuchar <zacharyk323@live.com>
2023-09-11 11:30:57 -0400

    Initial home setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Winterworks</h1>
+      <h4>Welcome to Winterworks</h4>
+      <h4>IDK what to put here: Maybe a picture</h4>
     </div>
   );
 }
