1bbcac765a42b943d5fb4f34d51c4517958391d9
Zachary Kuchar <zacharyk323@live.com>
2023-09-12 18:39:08 -0400

    Update contact info

--- src/pages/Contact/ContactInfo.jsx
+++ src/pages/Contact/ContactInfo.jsx
@@ -4,6 +4,8 @@ function ContactInfo() {
   return (
     <div>
       <h1>See our contact Info</h1>
+      <h1>Email: Winterworks@gmail.com</h1>
+      <h1>Phone: 412-111-1111</h1>
     </div>
   )
 }
