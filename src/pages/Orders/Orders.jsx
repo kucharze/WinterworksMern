a3244fa3d2d0fb8225c268464105e7dfc93c78d8
Zachary Kuchar <zacharyk323@live.com>
2023-09-14 14:39:24 -0400

    Inital form for submitting orders

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -4,6 +4,14 @@ function Orders() {
   return (
     <div>
       <h1>Page for placing orders</h1>
+      <form>
+        <input placeholder='Name'/> <br/>
+        <input placeholder='Phone Number'/><br/>
+        <input placeholder='Email'/><br/>
+        <input placeholder='Item'/><br/>
+        <input placeholder='Design'/><br/>
+        <input type='submit'/>
+      </form>
     </div>
   )
 }
