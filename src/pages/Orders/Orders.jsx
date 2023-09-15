da1b02eb52400a737072da6cd028bd268bdda240
Zachary Kuchar <zacharyk323@live.com>
2023-09-15 12:20:33 -0400

    State for for vars

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -8,18 +8,19 @@ function Orders() {
   const [item,setItem] = useState('')
   const [design,setDesign] = useState('')
 
-    const handelSubmit = (e) =>{
+    const handleSubmit = (e) =>{
         e.preventDefault();
+        console.log(name)
     }
   return (
     <div>
       <h1>Page for placing orders</h1>
-      <form>
-        <input placeholder='Name'/> <br/>
-        <input placeholder='Phone Number'/><br/>
-        <input placeholder='Email'/><br/>
-        <input placeholder='Item'/><br/>
-        <input placeholder='Design'/><br/>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e)=>{setName(e.target.value)}} placeholder='Name'/> <br/>
+        <input onChange={(e)=>{setPhoneNumber(e.target.value)}} placeholder='Phone Number'/><br/>
+        <input onChange={(e)=>{setEmail(e.target.value)}} placeholder='Email'/><br/>
+        <input onChange={(e)=>{setItem(e.target.value)}} placeholder='Item'/><br/>
+        <input onChange={(e)=>{setDesign(e.target.value)}} placeholder='Design'/><br/>
         <input type='submit'/>
       </form>
     </div>
