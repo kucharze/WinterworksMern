ba0af48c31efe731c6dfc740322bbfdbeb998f9d
Zachary Kuchar <zacharyk323@live.com>
2023-09-18 10:54:29 -0400

    Initial logic to try and submit to database

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const Orders = require("../models/orders");
+
 router.post("/", (req, res) => {
   console.log("Found The order post route");
   console.log("The sent body is ", req.body);
   res.json(req.body);
+
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(400).json("An error occured while trying to submit an order");
+  }
 });
 
 module.exports = router;
