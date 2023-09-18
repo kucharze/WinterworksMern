910e670f074a7d846ea2b98af5635a3940de2445
Zachary Kuchar <zacharyk323@live.com>
2023-09-18 12:52:04 -0400

    Changes to order submission

--- routes/orders.js
+++ routes/orders.js
@@ -6,12 +6,13 @@ const Orders = require("../models/orders");
 router.post("/", (req, res) => {
   console.log("Found The order post route");
   console.log("The sent body is ", req.body);
-  res.json(req.body);
+  //   res.json(req.body);
 
   try {
+    console.log("Attempting to create a new order");
     Orders.create(req.body);
   } catch (error) {
-    console.log(error);
+    console.log(error._message);
     res.status(400).json("An error occured while trying to submit an order");
   }
 });
