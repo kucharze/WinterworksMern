979e1142e9921493afd1e28adae2b93669bb7090
Zachary Kuchar <zacharyk323@live.com>
2023-09-19 11:09:55 -0400

    changes to order creation as well as success message

--- routes/orders.js
+++ routes/orders.js
@@ -3,14 +3,15 @@ const router = express.Router();
 
 const Orders = require("../models/orders");
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   console.log("Found The order post route");
   console.log("The sent body is ", req.body);
   //   res.json(req.body);
 
   try {
     console.log("Attempting to create a new order");
-    Orders.create(req.body);
+    await Orders.create(req.body);
+    res.status(200).json("Successful creation of an order");
   } catch (error) {
     console.log(error._message);
     res.status(400).json("An error occured while trying to submit an order");
