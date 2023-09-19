d5657cf45a4ab6b0eb2bf1f18b92c6f4372bac15
Zachary Kuchar <zacharyk323@live.com>
2023-09-19 15:14:12 -0400

    Change to elements

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,8 +5,9 @@ function About() {
   return (
     <div className={styles.About}>
       <h1>About page</h1>
-      <h1>Order rules</h1>
+      
       <div className={styles.list}>
+        <h1>Order rules</h1>
         <ul>
           <li>Please allow at least one week for order completion. These orders take time to complete</li>
           <li>Please use a valid email or phone number when submitting an order.  
