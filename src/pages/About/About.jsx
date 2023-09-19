49358fec91a751be1b8c47ea74c4727f2723261a
Zachary Kuchar <zacharyk323@live.com>
2023-09-19 13:09:59 -0400

    Added Rules section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styles from './About.module.css'
 
 function About() {
   return (
-    <div>
+    <div className={styles.About}>
       <h1>About page</h1>
+      <h1>Order rules</h1>
+      <div className={styles.list}>
+        <ul>
+          <li>Please allow at least one week for order completion. These orders take time to complete</li>
+          <li>Please use a valid email or phone number when submitting an order.  
+            If we cannot contact you, we will not put your name in the order queue.
+          </li>
+
+        </ul>
+      </div>
+      
     </div>
   )
 }
