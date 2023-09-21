4e58737334628b9b65420a3ad8e494489976240a
Zachary Kuchar <zacharyk323@live.com>
2023-09-21 14:12:47 -0400

    Added pictures to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import styles from './Home.module.css'
+import baby from '../../images/03 19 12_Baby Bib Quotes/03 19 12 001.jpg'
+import car from '../../images/10 12 12_Carhops/10 12 12 001.jpg'
 
 function Home() {
   return (
     <div>
         <h1>The home page</h1>
         <h2>Welcome to Winterworks</h2>
-        <h3>IDK what to put here: Maybe a picture</h3>
+        <h3>Check out our designs</h3>
+        <img src={baby} alt="" />
+        <img src={car} alt="" />
     </div>
   )
 } 
