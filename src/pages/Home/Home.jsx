f5b88890880e018d044c61c6c9a7f9eb177843af
Zachary Kuchar <zacharyk323@live.com>
2023-09-21 15:02:27 -0400

    New carhops pics

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import styles from './Home.module.css'
 import baby from '../../images/03 19 12_Baby Bib Quotes/03 19 12 001.jpg'
 import car from '../../images/10 12 12_Carhops/10 12 12 001.jpg'
 import car2 from '../../images/10 12 12_Carhops/10 12 12 004.jpg'
+import car3 from '../../images/10 12 12_Carhops/10 12 12 005.jpg'
 import cash from '../../images/04 26 12_ATM Cash World/04 16 12 001.jpg'
 import cash2 from '../../images/04 26 12_ATM Cash World/04 16 12 003.jpg'
 import cash3 from '../../images/04 26 12_ATM Cash World/04 16 12 002.jpg'
@@ -19,6 +20,7 @@ function Home() {
         <h1>Carhops</h1>
         <img src={car} alt="" />
         <img src={car2} alt="" />
+        <img src={car3} alt="" />
         <h1>ATM Cash World</h1>
         <img src={cash} alt="" />
         <img src={cash2} alt="" />
