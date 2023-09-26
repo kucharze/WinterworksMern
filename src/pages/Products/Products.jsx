a0f36de73d0a7986c2f425af4eef9554b199642a
Zachary Kuchar <zacharyk323@live.com>
2023-09-26 11:19:45 -0400

    Added content and styles to Products page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from './Products.module.css'
 
 function Products() {
   return (
-    <div>
+    <div className={styles.list}>
       <h1>See our Products</h1>
+      <div >
+        <h3>We sell a number of different products including:</h3>
+        <ul>
+          <li>Blankets</li>
+          <li>Clothing</li>
+        </ul>
+      </div>
+      
     </div>
   )
 }
