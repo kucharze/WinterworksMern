85f80276e72d29c95910c7ffdd11bbba61a1076c
Zachary Kuchar <zacharyk323@live.com>
2023-09-28 13:57:36 -0400

    Order rules to about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,6 +5,19 @@ function About() {
   return (
     <div className={styles.About}>
       <h1>About page</h1>
+      <p>Have any questions, check out out Contact info and shoot us a call or email.</p>
+
+      <div className={styles.list}>
+        <h2>How does ordering work?</h2>
+        <ol>
+          <li>Browse our products to see what is in stock</li>
+          <li>Go to our Orders page</li>
+          <li>Submit your order.  Including a description of the design you want, 
+            as well as a valid email and phone number.
+          </li>
+        </ol>
+      </div>
+      
       
       <div className={styles.list}>
         <h1>Order rules</h1>
