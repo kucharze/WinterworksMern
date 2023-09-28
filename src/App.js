2c0e5565d719d1f931b51c4fed8123b0b7bf424a
Zachary Kuchar <zacharyk323@live.com>
2023-09-28 14:23:47 -0400

    Removed design page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Products from "./pages/Products/Products";
 import Home from "./pages/Home/Home";
 import ContactInfo from "./pages/Contact/ContactInfo";
 import About from "./pages/About/About";
-import Designs from "./pages/Designs/Designs";
+// import Designs from "./pages/Designs/Designs";
 import Orders from "./pages/Orders/Orders";
 import title from "./images/WinterWorks.JPG";
 
@@ -18,7 +18,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/designs" element={<Designs />} />
         <Route path="/products" element={<Products />} />
         <Route path="/placeOrder" element={<Orders />} />
         <Route path="/contactinfo" element={<ContactInfo />} />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     <div className={styles.Navbar}>
         <Link to={'/'}>Home</Link>
         <Link to={'/about'}>About us</Link>
-        <Link to={'/designs'}>Designs</Link>
+        {/* <Link to={'/designs'}>Designs</Link> */}
         <Link to={'/products'}>Products</Link>
          <Link to={'/placeOrder'}>Place an Order</Link>
         <Link to={'/contactinfo'}>Contact Info</Link>
