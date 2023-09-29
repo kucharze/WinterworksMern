33ff20b052ccb1385014fb2ab5b0abab4363aa45
Zachary Kuchar <zacharyk323@live.com>
2023-09-29 14:56:47 -0400

    New styles for contact info page

--- src/pages/Contact/ContactInfo.jsx
+++ src/pages/Contact/ContactInfo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './Contact.module.css'
 
 function ContactInfo() {
   return (
-    <div>
+    <div className={styles.Contact}>
       <h1>See our contact Info</h1>
       <h1>Email: Winterworks@gmail.com</h1>
-      <h1>Phone: 412-111-1111</h1>
+      <h1>Phone: 412-884-5278</h1>
     </div>
   )
 }
