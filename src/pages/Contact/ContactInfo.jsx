244827b33e4faf29a394833649d3005a2dbd76c0
Zachary Kuchar <zacharyk323@live.com>
2023-11-01 13:55:32 -0400

    Changed elements and styles

--- src/pages/Contact/ContactInfo.jsx
+++ src/pages/Contact/ContactInfo.jsx
@@ -4,7 +4,7 @@ import styles from './Contact.module.css'
 function ContactInfo() {
   return (
     <div className={styles.Contact}>
-      <h1>See our contact Info</h1>
+      <h1>Contact Info</h1>
       <h1>Email: Winterworks@gmail.com</h1>
       <h1>Phone: 412-884-5278</h1>
     </div>
