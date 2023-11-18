f4b9ae80adb79bf4877fb820564a8f18a98617bc
Zachary Kuchar <zacharyk323@live.com>
2023-11-18 10:17:25 -0500

    Adjusted logo pictures and background

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import title from "../../images/WW Page 1.png";
+import title from "../../images/WinterWorks.JPG";
+import styles from './Logo.module.css'
 
 function Logo() {
   return (
-    <div>
-      <h1>The Logo</h1>
+    <div className={styles.LOGO}>
       <img src={title} alt="" />
     </div>
   )
