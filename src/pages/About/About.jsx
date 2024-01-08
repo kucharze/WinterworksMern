b326b8299480be2ba37bc6a5f405bca1c7bb0a2c
Zachary Kuchar <zacharyk323@live.com>
2024-01-07 19:25:15 -0500

    Adjusted styles

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,11 +7,12 @@ function About() {
       <h1>All created from a mom who likes to sow.</h1>
       <p>Have any questions, check out out Contact info and shoot us a call or email.</p>
 
-      
       <div className={styles.list}>
         <ol>
           <li>Our products are purchased from various venders.  Availability of specific items can depend on what they have in stock.</li>
+          <br/>
           <li>Go to our Orders page for questions.</li>
+          <br/>
           <li>Submit your order.  Including a description of the design you want, 
             as well as a valid email and phone number.
           </li>
