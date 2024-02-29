585a1d3265c556f4fcb60a60c9368f97b02cdcdc
Zachary Kuchar <zacharyk323@live.com>
2024-02-29 11:26:45 -0500

    New logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import title from "../../images/WinterWorks.JPG";
+// import title from "../../images/WinterWorks.JPG";
+import title from "../../images/Ww3.PNG";
 import styles from './Logo.module.css'
 
 function Logo() {
