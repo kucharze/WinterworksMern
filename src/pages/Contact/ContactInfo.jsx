abfafb379fb7c7f318a99216a806f599fcd08c15
Zachary Kuchar <zacharyk323@live.com>
2024-03-01 10:09:42 -0500

    Change to email in contacts

--- src/pages/Contact/ContactInfo.jsx
+++ src/pages/Contact/ContactInfo.jsx
@@ -5,7 +5,7 @@ function ContactInfo() {
   return (
     <div className={styles.Contact}>
       <h1>Contact Info</h1>
-      <h1>Email: Winterworks1@gmail.com</h1>
+      <h1>Email: winterworks1@gmail.com</h1>
       <h1>Phone: 412-884-5278</h1>
     </div>
   )
