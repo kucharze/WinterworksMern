bfc31976bbbfebbc685c3b69b0ef12927b33597a
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-02-01 21:44:47 -0500

    Add new Logo plus changes to styles

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import title from "../../images/WinterWorks.JPG";
+import title from "../../images/logos/WW Head.png";
 // import title from "../../images/Ww3.PNG";
 import styles from './Logo.module.css'
 
 function Logo() {
   return (
     <div className={styles.LOGO}>
-      <img src={title} alt="" className={styles.title}/>
+      {/* <img src={title} alt="" className={styles.title}/> */}
     </div>
   )
 }
