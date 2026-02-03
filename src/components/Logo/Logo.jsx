bbf7e2cf8b8e917be83a4d0412cd0279c710ccd1
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-02-02 21:13:13 -0500

    Set up new image and adjusted styles to fit

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import title from "../../images/logos/WW Head.png";
+import title from "../../images/logos/WWL wood1.png";
 // import title from "../../images/Ww3.PNG";
 import styles from './Logo.module.css'
 
 function Logo() {
   return (
     <div className={styles.LOGO}>
-      {/* <img src={title} alt="" className={styles.title}/> */}
+      <img src={title} alt="" className={styles.title}/>
     </div>
   )
 }
