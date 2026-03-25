006f378bcff615e2d4b697cc2ccb6c34a509f2d8
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-03-25 10:20:13 -0400

    Changed Navbar options to match footer buttons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import styles from './Navbar.module.css'
 function Navbar() {
   return (
     <div className={styles.Navbar}>
-        <Link className={styles.navbtn} to={'/'}>Home</Link>
+        <Link className={styles.navbtn} to={'/'}>Gallery</Link>
         <Link className={styles.navbtn} to={'/about'}>Info</Link>
         {/* <Link to={'/designs'}>Designs</Link> */}
         {/* <Link to={'/products'}>Products</Link> */}
